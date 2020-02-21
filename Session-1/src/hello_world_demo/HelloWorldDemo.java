7e97cd186e7560fe27e2b21053e1c4bd2603c626
Patricia <patriciapopa1997@gmail.com>
2020-02-21 18:19:04 +0200

    session 1

---
+++ Session-1/src/hello_world_demo/HelloWorldDemo.java
@@ -0,0 +1,22 @@
+package hello_world_demo;
+
+public class HelloWorldDemo {
+	public static void main(String[] args) {
+		// comentariu pe o linie
+
+		/* multi-line comment
+		 * pe mai multe
+		 * linii */
+
+		/**
+		 * javadoc comment
+		 */
+
+		// executia codului incepe intotdeauna din metoda main() si codul se executa linie cu line, de sus in jos
+
+		// println afiseaza o linie noua dupa ce a afisat textul pasat ca parametru
+		System.out.print("Hello, friend!");
+		System.out.println("Hello, dear friend!");
+		System.out.print("Hello, very dear friend!");
+	}
+}
